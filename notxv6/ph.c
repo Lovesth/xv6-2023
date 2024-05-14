5031a9fa060b008a69998da9b6e275d60a437edc
Minghao Xu <1667286082@qq.com>
2024-05-14 10:26:32 +0800

    passed the thread lab

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -14,6 +14,7 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+pthread_mutex_t locks[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
@@ -43,6 +44,9 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
+  // The invariant is that insert the key if the key doesn't exist, update it's value if the key exist.
+  // So lock must be here.
+  pthread_mutex_lock(&locks[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
@@ -54,7 +58,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&locks[i]);
 }
 
 static struct entry*
@@ -118,6 +122,10 @@ main(int argc, char *argv[])
     keys[i] = random();
   }
 
+  // init the locks
+  for(int i=0; i<NBUCKET; i++){
+    pthread_mutex_init(&locks[i], NULL);
+  }
   //
   // first the puts
   //
